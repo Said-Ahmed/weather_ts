12f38ae99090a7275821f17cfb8868a1016db7f9
Said-Ahmed <akhmed_kurb@mail.ru>
2025-05-27 23:41:55 +0300

    Final commit

--- src/database.py
+++ src/database.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import DeclarativeBase
 DB_HOST = 'localhost'
 DB_PORT = 5432
 DB_USER = 'postgres'
-DB_PASS = 'dag05307'
-DB_NAME = 'weather'
+DB_PASS = 'postgres'
+DB_NAME = 'postgres'
 
 DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 DATABASE_PARAMS = {}
